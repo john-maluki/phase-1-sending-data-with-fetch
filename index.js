de0bc35bf7d2918a3abfad5d374e3123c42cf85e
john <johnmaluki58@gmail.com>
2023-06-26 19:20:15 +0300

    Complete assignment on Sending Data with Fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+const submitData = (name, email) => {
+  const data = {
+    name: name,
+    email: email,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((resp) => resp.json())
+    .then((data) => {
+      document.querySelector("body").textContent = data.id;
+    })
+    .catch((err) => {
+      document.querySelector("body").textContent = err.message;
+    });
+};
